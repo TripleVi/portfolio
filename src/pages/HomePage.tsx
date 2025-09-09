7ffd6febb4889e4807760fa024a44e5e08a267bc
Vuong Vu <vuongvu061101@gmail.com>
2025-09-09 22:02:56 +0700

    init project

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h2 className="text-primary">Hello world</h2>
+}
+
+export default HomePage
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,6 @@
+import HomePage from "@/pages/HomePage"
+
+
+const AppRoutes = () => {
+    return <HomePage />
+}
